7d7974ed4f44229084e80e456198e91953b301f4
Lee <leejavaa@gmail.com>
2022-11-18 07:09:23 +0200

    NEF-2-Finished Landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,55 @@
 module.exports = {
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBackground: '#0F0F12',
+        light: '#D9D9D9',
+      },
+      fontFamily: {
+        Spectral: ['Spectral', 'serif'],
+        IBM: ['IBM Plex Sans', 'sans-serif'],
+      },
+      fontSize: {
+        landing: '11.4rem',
+      },
+      animation: {
+        fadeInOut: 'fadeInOut 2s ease-in-out',
+        fadeIn: 'fadeIn 2s ease-in',
+        floatUp: 'floatUp 3s ease-out',
+      },
+      keyframes: {
+        fadeInOut: {
+          '0%, 100%': { opacity: 0 },
+          '25%, 75%': { opacity: 1 },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '50%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        floatUp: {
+          '0%': {
+            opacity: 0,
+          },
+          '40%': {
+            opacity: 0,
+            transform: 'translateY(80%)',
+          },
+          '60%': {
+            opacity: 0,
+          },
+          '80%': {
+            opacity: 0.8,
+            transform: 'translateY(0%)',
+          },
+          '100%': {
+            opacity: 0.8,
+            transform: 'translateY(0%)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
