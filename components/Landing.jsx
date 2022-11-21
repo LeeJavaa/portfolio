b98a7721c8b826414ef23dba82ff593a54c54695
LeeJavaa <leejavaa@gmail.com>
2022-11-21 20:27:50 +0200

    NEF-2-E2E Testing Setup

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Landing = () => (
   <main className="grid place-content-center">
-    <div className="w-screen h-screen relative overflow-hidden">
+    <div className="w-screen h-screen relative overflow-hidden" id='backgroundImage'>
       <Image
         src="/images/GradientLanding.gif"
         fill="true"
@@ -17,18 +17,20 @@ const Landing = () => (
       <Link
         href="/"
         className="text-light text-2xl lg:text-4xl font-bold animate-subtleFloat"
+        id='navLogo'
       >
         Nefelibata.
       </Link>
       <div
         role="menu"
         className=" w-8 lg:w-10 h-11 cursor-pointer grid justify-center items-center relative animate-subtleFloat"
+        id='navMenu'
       >
         <div className="absolute right-0 w-2/3 h-1 lg:h-1.5 bg-light before:content-[''] after:content-[''] before:absolute after:absolute before:w-8 before:lg:w-10 before:right-0 before:h-1 before:lg:h-1.5 after:w-1/2 after:right-0 after:h-1 after:lg:h-1.5 before:bg-light after:bg-light before:-translate-y-2 after:translate-y-2 before:lg:-translate-y-3 after:lg:translate-y-3" />
       </div>
     </nav>
     <section className="absolute w-screen h-full grid place-items-center">
-      <h1 className="text-center text-5xl lg:text-9xl 2xl:text-landing font-extrabold font-Spectral mx-auto w-full text-white opacity-80 animate-floatUp">
+      <h1 className="text-center text-5xl lg:text-9xl 2xl:text-landing font-extrabold font-Spectral mx-auto w-full text-white opacity-80 animate-floatUp" id='heroText'>
         Lee Johnson
       </h1>
     </section>
